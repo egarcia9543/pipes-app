3c3b51ca89da59a090e46146e1d31d2d02f98b0b
esteban.garcia2 <esteban.garcia@pragma.com.co>
2024-05-12 02:04:06 -0500

    Add i18n pipes and async pipe to uncommon component

--- src/app/components/pages/uncommon/uncommon.component.ts
+++ src/app/components/pages/uncommon/uncommon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon.component.css'
 })
 export class UncommonComponent {
+  //i18n Select
+  public name: string = 'Arnold';
+  public gender: 'male' | 'female' = 'male'
+  public invitationMap = {
+    'male': 'Conocerlo',
+    'female': 'Conocerla'
+  }
 
+  changeClient():void {
+    this.name = 'Alice';
+    this.gender = 'female';
+  }
+
+  //i18n Plural
+  public clients: string[] = ['Arnold', 'Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Helen', 'Ivan'];
+  public clientsMap = {
+    '=0': 'No hay clientes',
+    '=1': 'Un cliente',
+    'other': '# clientes'
+  }
+
+  deleteClient():void {
+    this.clients.pop();
+  };
+
+
+  //Key-Value Pipe
+  public person = {
+    name: 'Arnold',
+    age: 25,
+    address: '1234 Main St'
+  }
+
+  //ASYNC Pipe
+  public myObservableTimer = interval(2000);
+
+  public promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Data has arrived')
+    }, 3000);
+  });
 }
